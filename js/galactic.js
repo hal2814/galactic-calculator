cb1c93d2b82dec6098febe6201610095670b7cfb
Nate McGregor <natecmcgregor@gmail.com>
2017-10-13 09:32:05 -0700

    create function skeletons

---
+++ js/galactic.js
@@ -0,0 +1,34 @@
+export class Galactic{
+  constructor(age){
+    this.age = age;
+    this.ageInSeconds = 0.0;
+  }
+
+  determineAge () {
+
+  }
+
+  convertSeconds () {
+
+  }
+
+  mercuryAge () {
+
+  }
+
+  venusAge () {
+
+  }
+
+  marsAge () {
+
+  }
+
+  jupiterAge () {
+
+  }
+
+  lifeExpectancy () {
+    
+  }
+}
