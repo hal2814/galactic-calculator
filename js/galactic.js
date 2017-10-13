be0cfccb221236463ed983640e8409136d98b84a
Nate McGregor <natecmcgregor@gmail.com>
2017-10-13 10:00:45 -0700

    write age functions

--- js/galactic.js
+++ js/galactic.js
@@ -1,34 +1,36 @@
 export class Galactic{
-  constructor(age){
+  constructor(age,birthday){
     this.age = age;
-    this.ageInSeconds = 0.0;
+    this.ageInSeconds = 0;
+    this.birthday = birthday;
   }
 
-  determineAge () {
+  determineAge (currentTime) {
 
   }
 
   convertSeconds () {
-
+    this.ageInSeconds = this.age * 31540000;
+    return this.ageInSeconds;
   }
 
   mercuryAge () {
-
+    let age = this.age * 0.24;
   }
 
   venusAge () {
-
+    let age = this.age * 0.62;
   }
 
   marsAge () {
-
+    let age = this.age * 1.88;
   }
 
   jupiterAge () {
-
+    let age = this.age * 11.86;
   }
 
   lifeExpectancy () {
-    
+
   }
 }
