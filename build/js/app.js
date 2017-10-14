8cee74c511fcc04697494b260470abf07b8fc5c3
Nate McGregor <natecmcgregor@gmail.com>
2017-10-13 17:16:14 -0700

    add images to planet selection

--- build/js/app.js
+++ build/js/app.js
@@ -16,6 +16,7 @@ var Galactic = exports.Galactic = function () {
     this.age = age;
     this.ageInSeconds = 0;
     this.birthday = birthday;
+    this.life = 0;
     //.replace(/-|\s/g,"") to remove dashes
   }
 
@@ -27,31 +28,51 @@ var Galactic = exports.Galactic = function () {
     }
   }, {
     key: "mercuryAge",
-    value: function mercuryAge() {
-      var age = parseFloat((this.age / 0.24).toFixed(1));
-      return age;
+    value: function mercuryAge(age) {
+      var planetAge = parseFloat((age / 0.24).toFixed(1));
+      return planetAge;
     }
   }, {
     key: "venusAge",
-    value: function venusAge() {
-      var age = parseFloat((this.age / 0.62).toFixed(1));
-      return age;
+    value: function venusAge(age) {
+      var planetAge = parseFloat((age / 0.62).toFixed(1));
+      return planetAge;
     }
   }, {
     key: "marsAge",
-    value: function marsAge() {
-      var age = parseFloat((this.age / 1.88).toFixed(1));
-      return age;
+    value: function marsAge(age) {
+      var planetAge = parseFloat((age / 1.88).toFixed(1));
+      return planetAge;
     }
   }, {
     key: "jupiterAge",
-    value: function jupiterAge() {
-      var age = parseFloat((this.age / 11.86).toFixed(1));
-      return age;
+    value: function jupiterAge(age) {
+      var planetAge = parseFloat((age / 11.86).toFixed(1));
+      return planetAge;
     }
   }, {
     key: "lifeExpectancy",
-    value: function lifeExpectancy() {}
+    value: function lifeExpectancy(cont, smoke) {
+      var age = 0;
+      if (cont === "North America") {
+        age = 81;
+      } else if (cont === "South America") {
+        age = 79;
+      } else if (cont === "Europe") {
+        age = 81;
+      } else if (cont === "Asia") {
+        age = 74;
+      } else if (cont === "Africa") {
+        age = 64;
+      } else if (cont === "Oceania") {
+        age = 79;
+      }
+      if (smoke === "Yes") {
+        age -= 10;
+      }
+      this.life = age;
+      return age;
+    }
   }]);
 
   return Galactic;
@@ -72,10 +93,13 @@ $(document).ready(function () {
     var mo = moment(birthday, "YYYY-MM-DD").fromNow();
     var age = parseInt(mo[0] + mo[1]);
     var planet = $('#selectplanet').val();
+    var cont = $('#selectContinent').val();
+    var smoke = $('#smoke').val();
     console.log("age: " + age + " ,type: " + (typeof age === 'undefined' ? 'undefined' : _typeof(age)));
     console.log("birthday: " + birthday + " ,type: " + (typeof birthday === 'undefined' ? 'undefined' : _typeof(birthday)));
 
     var newGalactic = new _galactic.Galactic(age, birthday);
+    newGalactic.lifeExpectancy(cont, smoke);
     console.log("this.age: " + newGalactic.age + " ,type: " + _typeof(newGalactic.age));
     console.log("newGalactic.birthday: " + newGalactic.birthday + " ,type: " + _typeof(newGalactic.birthday));
     // $('#output1').text(newGalactic.age);
@@ -84,38 +108,43 @@ $(document).ready(function () {
       $('#output1earth').text(newGalactic.age);
       $('#output2earth').text(newGalactic.ageInSeconds);
       $('#output3earth').text(newGalactic.birthday);
+      $('#output4earth').text(newGalactic.life);
       $('#resultEarth').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
       $('#resultMars').hide();
       $('#resultJupiter').hide();
     } else if (planet === "Mercury") {
-      $('#output1merc').text(newGalactic.mercuryAge());
+      $('#output1merc').text(newGalactic.mercuryAge(newGalactic.age));
       $('#output3merc').text(newGalactic.birthday);
+      $('#output4merc').text(newGalactic.mercuryAge(newGalactic.life));
       $('#resultMercury').show();
       $('#resultVenus').hide();
       $('#resultMars').hide();
       $('#resultJupiter').hide();
       $('#resultEarth').hide();
     } else if (planet === "Venus") {
-      $('#output1ven').text(newGalactic.venusAge());
+      $('#output1ven').text(newGalactic.venusAge(newGalactic.age));
       $('#output3ven').text(newGalactic.birthday);
+      $('#output4ven').text(newGalactic.venusAge(newGalactic.life));
       $('#resultVenus').show();
       $('#resultMercury').hide();
       $('#resultMars').hide();
       $('#resultJupiter').hide();
       $('#resultEarth').hide();
     } else if (planet === "Mars") {
-      $('#output1mars').text(newGalactic.marsAge());
+      $('#output1mars').text(newGalactic.marsAge(newGalactic.age));
       $('#output3mars').text(newGalactic.birthday);
+      $('#output4mars').text(newGalactic.marsAge(newGalactic.life));
       $('#resultMars').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
       $('#resultJupiter').hide();
       $('#resultEarth').hide();
     } else if (planet === "Jupiter") {
-      $('#output1jup').text(newGalactic.jupiterAge());
+      $('#output1jup').text(newGalactic.jupiterAge(newGalactic.age));
       $('#output3jup').text(newGalactic.birthday);
+      $('#output4jup').text(newGalactic.jupiterAge(newGalactic.life));
       $('#resultJupiter').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
     let mo = moment(birthday, "YYYY-MM-DD").fromNow();
     let age = parseInt(mo[0]+mo[1]);
     let planet = $('#selectplanet').val();
+    let cont = $('#selectContinent').val();
+    let smoke = $('#smoke').val();
     console.log("age: "+ age + " ,type: "+ typeof age);
     console.log("birthday: "+ birthday + " ,type: "+typeof birthday);
 
     let newGalactic = new Galactic(age,birthday);
+    newGalactic.lifeExpectancy(cont,smoke);
     console.log("this.age: "+ newGalactic.age + " ,type: "+ typeof newGalactic.age);
     console.log("newGalactic.birthday: "+ newGalactic.birthday + " ,type: "+typeof newGalactic.birthday);
     // $('#output1').text(newGalactic.age);
@@ -20,6 +23,7 @@ $(document).ready(function() {
       $('#output1earth').text(newGalactic.age);
       $('#output2earth').text(newGalactic.ageInSeconds);
       $('#output3earth').text(newGalactic.birthday);
+      $('#output4earth').text(newGalactic.life);
       $('#resultEarth').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
@@ -27,8 +31,9 @@ $(document).ready(function() {
       $('#resultJupiter').hide();
     }
     else if(planet === "Mercury"){
-      $('#output1merc').text(newGalactic.mercuryAge());
+      $('#output1merc').text(newGalactic.mercuryAge(newGalactic.age));
       $('#output3merc').text(newGalactic.birthday);
+      $('#output4merc').text(newGalactic.mercuryAge(newGalactic.life));
       $('#resultMercury').show();
       $('#resultVenus').hide();
       $('#resultMars').hide();
@@ -36,8 +41,9 @@ $(document).ready(function() {
       $('#resultEarth').hide();
     }
     else if(planet === "Venus"){
-      $('#output1ven').text(newGalactic.venusAge());
+      $('#output1ven').text(newGalactic.venusAge(newGalactic.age));
       $('#output3ven').text(newGalactic.birthday);
+      $('#output4ven').text(newGalactic.venusAge(newGalactic.life));
       $('#resultVenus').show();
       $('#resultMercury').hide();
       $('#resultMars').hide();
@@ -45,8 +51,9 @@ $(document).ready(function() {
       $('#resultEarth').hide();
     }
     else if(planet === "Mars"){
-      $('#output1mars').text(newGalactic.marsAge());
+      $('#output1mars').text(newGalactic.marsAge(newGalactic.age));
       $('#output3mars').text(newGalactic.birthday);
+      $('#output4mars').text(newGalactic.marsAge(newGalactic.life));
       $('#resultMars').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
@@ -54,8 +61,9 @@ $(document).ready(function() {
       $('#resultEarth').hide();
     }
     else if(planet === "Jupiter"){
-      $('#output1jup').text(newGalactic.jupiterAge());
+      $('#output1jup').text(newGalactic.jupiterAge(newGalactic.age));
       $('#output3jup').text(newGalactic.birthday);
+      $('#output4jup').text(newGalactic.jupiterAge(newGalactic.life));
       $('#resultJupiter').show();
       $('#resultMercury').hide();
       $('#resultVenus').hide();
